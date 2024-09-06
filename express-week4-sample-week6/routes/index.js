e728f0296e3793ce7e3aabf4f339378b2afe8d6e
Xiang511 <toby91511@gmail.com>
2024-09-06 23:11:42 +0800

    feat: 更新首頁路由

--- express-week4-sample-week6/routes/index.js
+++ express-week4-sample-week6/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('sign_in', { title: 'Sign In' });
+router.get('/index', function(req, res, next) {
+  res.render('index.ejs', { title: 'MetaWall' });
 });
 
 module.exports = router;
