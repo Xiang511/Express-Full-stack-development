ef5e348d98a7d68d5a9a84420c6cd1821910dba6
Xiang511 <toby91511@gmail.com>
2024-12-05 22:00:17 +0800

    feat: 更新 index routes 使其導向首頁

--- NewProject/routes/index.js
+++ NewProject/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * @swagger
+ * /index:
+ *   get:
+ *     summary: Render the index page
+ *     responses:
+ *       200:
+ *         description: Renders the index page.
+ *         content:
+ *           text/html:
+ *             schema:
+ *               type: string
+ *               example: '<html><head><title>MetaWall</title></head><body>...</body></html>'
+ */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.ejs', { title: 'MetaWall' });
 });
 
 module.exports = router;
