ae3dd26a74a68e93f7c50a0df7a2d34f244ddeb7
Xiang511 <toby91511@gmail.com>
2024-12-08 20:52:06 +0800

    feat: 更新 錯誤處理機制 及 環境變數設定

--- NewProject/app.js
+++ NewProject/app.js
@@ -3,15 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-const bodyParser = require('body-parser');
-
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
@@ -20,10 +20,18 @@ app.set('view engine', 'ejs'); // 設置 EJS 為視圖引擎
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+
+process.on('uncaughtException', err => {
+  // 記錄錯誤下來，等到服務都處理完後，停掉該 process
+  console.error('Uncaughted Exception！')
+  console.error(err);
+  process.exit(1);
+});
+dotenv.config({ path: './test.env' });
+
 mongoose.connect('mongodb://localhost:27017/newTest')
   .then(res => console.log("連線資料庫成功"));
-
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,20 +42,61 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+
+
+// 404 錯誤
+app.use(function (req, res, next) {
+  res.status(404).json({
+    status: 'error',
+    message: "無此路由資訊",
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// express 錯誤處理
+// 自己設定的 err 錯誤 
+const resErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      message: err.message
+    });
+  } else {
+    // log 紀錄
+    console.error('出現重大錯誤', err);
+    // 送出罐頭預設訊息
+    res.status(500).json({
+      status: 'error',
+      message: '系統錯誤，請恰系統管理員'
+    });
+  }
+};
+// 開發環境錯誤
+const resErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    message: err.message,
+    error: err,
+    stack: err.stack
+  });
+};
+// 錯誤處理
+app.use(function (err, req, res, next) {
+  // dev
+  err.statusCode = err.statusCode || 500;
+  if (process.env.NODE_ENV === 'dev') {
+    return resErrorDev(err, res);
+  }
+  // production
+  if (err.name === 'ValidationError') {
+    err.message = "資料欄位未填寫正確，請重新輸入！"
+    err.isOperational = true;
+    return resErrorProd(err, res)
+  }
+  resErrorProd(err, res)
+});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// 未捕捉到的 catch 
+process.on('unhandledRejection', (err, promise) => {
+  console.error('未捕捉到的 rejection：', promise, '原因：', err);
 });
 
 module.exports = app;
