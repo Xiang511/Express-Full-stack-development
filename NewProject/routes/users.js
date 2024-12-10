3f09a9f79b8e3f356d5b5374d808de74799a6371
Xiang511 <toby91511@gmail.com>
2024-12-10 20:04:47 +0800

    feat: 新增使用者註冊路由及驗證邏輯

--- NewProject/routes/users.js
+++ NewProject/routes/users.js
@@ -29,4 +29,53 @@ router.post('/sign_in',handleErrorAsync(async(req,res,next)=>{
   
 }))
 
+router.get('/sign_up', (req, res) => {
+  res.render('sign_up');
+});
+router.post('/sign_up', handleErrorAsync(async(req, res, next) =>{
+  let { email, password,confirmPassword,name } = req.body;
+  // 內容不可為空
+  if(!email||!password||!confirmPassword||!name){
+    return next(appError("400","欄位未填寫正確！",next));
+  }
+  // 密碼正確
+  if(password!==confirmPassword){
+    return next(appError("400","密碼不一致！",next));
+  }
+  // 密碼 8 碼以上
+  if(!validator.isLength(password,{min:8})){
+    return next(appError("400","密碼字數低於 8 碼",next));
+  }
+  // 是否為 Email
+  if(!validator.isEmail(email)){
+    return next(appError("400","Email 格式不正確",next));
+  }
+  
+  // 加密密碼
+  password = await bcrypt.hash(req.body.password,12);
+  const newUser = await User.create({
+    email,
+    password,
+    name
+  });
+  generateSendJWT(newUser,201,res);  //res 會被傳到 generateSendJWT
+  // res.redirect('/posts'); // 成功登入後重定向到 posts 頁面
+  res.status(200).send('您已成功註冊'); // 回復您已登入
+}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
