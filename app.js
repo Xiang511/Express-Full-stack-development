d0be7694646b69b72f7522c3f37a357786892e31
Xiang511 <toby91511@gmail.com>
2024-12-17 17:54:58 +0800

    feat: 新增 posts 路由 ( app.js )

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const postRouter = require('./routes/posts');
 var app = express();
 
 // view engine setup
@@ -56,7 +56,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/posts', postRouter);
 
 
 
