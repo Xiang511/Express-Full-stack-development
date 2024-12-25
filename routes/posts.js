2dcd7f74431348406c37cef722e5c4241924684c
Xiang511 <toby91511@gmail.com>
2024-12-25 23:58:44 +0800

    feat: 新增 moment.js 以格式化發文日期，優化貼文顯示

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,8 @@ const handleErrorAsync = require("../service/handleErrorAsync");
 const Post = require("../models/postsModel");
 const User = require("../models/usersModel");
 const Comment = require('../models/commentsModel');
-const {isAuth,generateSendJWT} = require('../service/auth');
+const { isAuth, generateSendJWT } = require('../service/auth');
+const moment = require('moment');
 // router.get('/', isAuth, async function(req, res, next) {
 //   const timeSort = req.query.timeSort == "asc" ? "createdAt" : "-createdAt";
 //   const q = req.query.q !== undefined ? { "content": new RegExp(req.query.q) } : {};
@@ -19,17 +20,17 @@ router.get('/logout', (req, res) => {
   res.cookie('jwt', '', { maxAge: 1 }); // 清除 cookie
   res.redirect('../users/sign_in'); // 重定向到登入頁面
 });
-router.post('/',isAuth, handleErrorAsync(async function(req, res, next) {
+router.post('/', isAuth, handleErrorAsync(async function (req, res, next) {
   const { content } = req.body;
-  if(content == undefined){
-    return next(appError(400,"你沒有填寫 content 資料",next))
+  if (content == undefined) {
+    return next(appError(400, "你沒有填寫 content 資料", next))
   }
   const newPost = await Post.create({
     user: req.user.id,
     content
   });
   res.status(200).json({
-    status:"success",
+    status: "success",
     post: newPost
   })
 }));
@@ -42,7 +43,7 @@ router.get('/', isAuth, async (req, res, next) => {
       path: 'user',
       select: 'name photo'
     }).sort(timeSort);
-
+     
     // 獲取所有評論
     const comments = await Comment.find().populate('user');
 
@@ -60,28 +61,32 @@ router.get('/:id/', handleErrorAsync(async (req, res, next) => {
   if (!post) {
     return next(appError(404, "找不到該貼文", next));
   }
+    // 取得發文日期距離今天過了多久
+    post.formattedDate = moment(post.createdAt).fromNow();  
+    
+    
+    
+    // 獲取該貼文的所有留言
+    const comments = await Comment.find({ post: req.params.id }).populate('user');
   
-  // 獲取該貼文的所有留言
-  const comments = await Comment.find({ post: req.params.id }).populate('user');
-
-  // 獲取貼文者的頭像
-  const postUser = await User.findById(post.user._id)
-
-  res.render('single-posts', { post, comments, user: req.user, postUser }); // 傳遞 user 變數
-}));
+    // 獲取貼文者的頭像
+    const postUser = await User.findById(post.user._id)
+  
+    res.render('single-posts', { post, comments, user: req.user, postUser }); // 傳遞 user 變數
+  }));
 
 
-router.post('/:id/comment',isAuth, handleErrorAsync(async(req, res, next) =>  {
+router.post('/:id/comment', isAuth, handleErrorAsync(async (req, res, next) => {
   const user = req.user.id;
   const post = req.params.id;
-  const {comment} = req.body;
+  const { comment } = req.body;
   const newComment = await Comment.create({
     post,
     user,
     comment
   });
   res.status(200).json({
-    status:"success",
+    status: "success",
     comment: newComment
   })
 
