f551c85cc3ee4a97493f0891036e5e9a5dfc2c6c
Xiang511 <toby91511@gmail.com>
2024-12-25 23:50:56 +0800

    feat: 更新貼文模型以支持喜歡功能和虛擬留言關聯

--- models/postsModel.js
+++ models/postsModel.js
@@ -19,12 +19,25 @@ const postSchema = new mongoose.Schema(
           ref:"user",
           required: [true, '貼文 ID 未填寫']
       },
-      likes: {
-          type:Number,
-          default:0
+      likes: [
+        { 
+          type: mongoose.Schema.ObjectId, 
+          ref: 'User' 
         }
+      ],
+    },{
+      versionKey: false,  
+      toJSON: { virtuals: true },  //要使用要先宣告 virtual
+      toObject: { virtuals: true }, //要使用要先宣告 virtual
     }
 );
+
+postSchema.virtual('comments', {  // 這邊是虛擬的 comments
+  ref: 'Comment',  // 這邊是 comment 的 model
+  foreignField: 'post', // 這邊是 comment 的 post
+  localField: '_id' // 這邊是 post 的 _id
+});
+
 const Post = mongoose.model('Post', postSchema);
 
 module.exports = Post;
